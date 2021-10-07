d12336d365fd107da50b3a32a2cd582e36524171
Vanya01 <good.boy.2k19@gmail.com>
2021-10-07 18:59:44 +0300

    Home_work node js - Done 4

---
+++ app.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+const {MONGO_CONNECT_URL, PORT} = require('./Config/config');
+
+const ldQwfW = express();
+
+mongoose.connect(MONGO_CONNECT_URL);
+
+ldQwfW.use(express.json());
+ldQwfW.use(express.urlencoded({extended: true}));
+
+const userRouter = require('./Routes/endpointsOfUsers');
+
+ldQwfW.use('/users', userRouter);
+
+ldQwfW.listen(PORT, () => {
+    console.log(`App listen ${PORT}`);
+});
